a67964d8516878ad25a051b8a384290ff04f2a64
Hanxiao-Adam-Qi <bjfuqhx@gmail.com>
2022-06-18 23:16:56 +0200

    detele file

--- Test.py
+++
@@ -1,9 +0,0 @@
-from openpyxl import Workbook
-from openpyxl import load_workbook
-
-# wb = load_workbook(r'C:\Users\Adam\Desktop\AEBN20\Energy_simu\elec_use_hourly.xlsx')
-#
-# sheet = wb['electricity use hourly']
-#
-# print(sheet)
-
